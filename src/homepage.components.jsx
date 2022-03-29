bb4db3bb5cadf14f2fa120fc66a70c771d3e0a0d
Gaben1987 <68336155+Gaben1987@users.noreply.github.com>
2022-03-30 00:07:05 +0200

    first update

---
+++ src/homepage.components.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import './homepage.styes.scss'
+
+const HomePage=()=>(
+
+<div className="homepage">
+<div className="directory-menu">
+    <div className="menu-item">
+        <div className="content">
+           <h1 className="title">SZEZONÁLIS TERMÉKEK</h1>
+           <span className="subtitle">SHOP NOW</span>
+
+        </div>
+
+    </div>
+    <div className="menu-item">
+        <div className="content">
+           <h1 className="title">HORGOLT TERMÉKEK</h1>
+           <span className="subtitle">SHOP NOW</span>
+
+        </div>
+
+    </div>
+    <div className="menu-item">
+        <div className="content">
+           <h1 className="title">DISZTÁRGYAK</h1>
+           <span className="subtitle">SHOP NOW</span>
+
+        </div>
+
+    </div>
+    <div className="menu-item">
+        <div className="content">
+           <h1 className="title">ALAPANYAGOK</h1>
+           <span className="subtitle">SHOP NOW</span>
+
+        </div>
+
+    </div>
+    <div className="menu-item">
+        <div className="content">
+           <h1 className="title">SZÁLLITÁSI INFOMÁCIÓK</h1>
+           <span className="subtitle"></span>
+
+        </div>
+
+    </div>
+</div>
+</div>
+)
+
+export default HomePage
